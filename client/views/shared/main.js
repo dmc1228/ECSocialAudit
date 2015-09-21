42e9d503b40b938a39f88af267304167e3ba3df6
Carla Goldstein <carla.goldstein@gmail.com>
2015-09-21 14:51:03 +0200

    layout on all screens now 100%

--- client/views/shared/main.js
+++ client/views/shared/main.js
@@ -1,6 +1,11 @@
 Template.header.onRendered(function() {
   // Initialize collapse button
-  $(".button-collapse").sideNav();
+  // $(".button-collapse").sideNav();
   // Initialize collapsible (uncomment the line below if you use the dropdown variation)
   //$('.collapsible').collapsible();
+  $('.button-collapse').sideNav({
+      // menuWidth: 300, // Default is 240
+      // closeOnClick: true // Closes side-nav on <a> clicks, useful for Angular/Meteor
+    }
+  );
 });
