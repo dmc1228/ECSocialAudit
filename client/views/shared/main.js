f6486f994057d2d544843d01afcf1cab962aa727
Carla Goldstein <carla.goldstein@gmail.com>
2015-09-21 15:08:56 +0200

    added side nav

--- client/views/shared/main.js
+++ client/views/shared/main.js
@@ -5,7 +5,7 @@ Template.header.onRendered(function() {
   //$('.collapsible').collapsible();
   $('.button-collapse').sideNav({
       // menuWidth: 300, // Default is 240
-      // closeOnClick: true // Closes side-nav on <a> clicks, useful for Angular/Meteor
+      closeOnClick: true // Closes side-nav on <a> clicks, useful for Angular/Meteor
     }
   );
 });
