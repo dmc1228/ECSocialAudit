1f2f571447b73e75c19375e6eb627b5000162149
Carla Goldstein <carla.goldstein@gmail.com>
2015-09-21 23:18:57 +0200

    added more sections to Form A

--- client/views/shared/main.js
+++ client/views/shared/main.js
@@ -1,21 +1,21 @@
-Template.header.onRendered(function() {
-  // Initialize collapse button
-  // $(".button-collapse").sideNav();
-  // Initialize collapsible (uncomment the line below if you use the dropdown variation)
-  //$('.collapsible').collapsible();
-  $('.button-collapse').sideNav({
-      // menuWidth: 300, // Default is 240
-      closeOnClick: true // Closes side-nav on <a> clicks, useful for Angular/Meteor
-    }
-  );
-});
+// Template.header.onRendered(function() {
+//   // Initialize collapse button
+//   // $(".button-collapse").sideNav();
+//   // Initialize collapsible (uncomment the line below if you use the dropdown variation)
+//   //$('.collapsible').collapsible();
+//   $('.button-collapse').sideNav({
+//       menuWidth: 300, // Default is 240
+//       closeOnClick: true // Closes side-nav on <a> clicks, useful for Angular/Meteor
+//     }
+//   );
+// });
 
 Template.sideNav.onRendered(function() {
   $('.collapsible').collapsible({
     accordion : false // A setting that changes the collapsible behavior to expandable instead of the default accordion style
   });
   $('.button-collapse').sideNav({
-      // menuWidth: 300, // Default is 240
+      menuWidth: 450, // Default is 240
       closeOnClick: true // Closes side-nav on <a> clicks, useful for Angular/Meteor
     }
   );
