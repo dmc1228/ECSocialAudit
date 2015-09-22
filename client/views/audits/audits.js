523c0189c6aea8625ca1c47997d030cfe1bfb4bf
Carla Goldstein <carla.goldstein@gmail.com>
2015-09-22 10:02:18 +0200

    added delete and update buttons to "My Audits" screen

--- client/views/audits/audits.js
+++ client/views/audits/audits.js
@@ -1,3 +1,36 @@
-Template.registerHelper("audits", function (param2) {
-  return Audits.find({});
+
+Template.manageAudits.helpers({
+    settings: function ()
+    {
+      return {
+          collection: Audits,
+          rowsPerPage: 10,
+          showFilter: true,
+          fields: [
+            { key: 'school.schoolDetails.INSTITUTION_NAME', label: 'Name' },
+            { key: 'school.schoolDetails.DISTRICT_NAME', label: 'District' },
+            { key: 'update', label: '', fn: function () { return new Spacebars.SafeString('<button type="button" class="editbtn">Update</button>') } },
+            { key: 'delete', label: '', fn: function () { return new Spacebars.SafeString('<button type="button" class="deletebtn">Delete</button>') } }
+
+          ]
+
+      };
+    }
+});
+
+Template.audits.events({
+  'click .reactive-table tbody tr': function (event) {
+    event.preventDefault();
+
+    // checks if the actual clicked element has the class `delete`
+    if (event.target.className == "deletebtn") {
+      Audits.remove(this._id)
+    }
+    else if (event.target.className == "editbtn") {
+      Router.go('audit.formA', {_id: this._id, _section: 'general'});
+    } else {
+      Router.go('audit.formA', {_id: this._id, _section: 'general'});
+    }
+  }
+
 });
