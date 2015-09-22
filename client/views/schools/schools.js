cd6e8e7f6bef4784439e352d00723176a5ca738b
Carla Goldstein <carla.goldstein@gmail.com>
2015-09-22 09:54:08 +0200

    can delete schools now

--- client/views/schools/schools.js
+++ client/views/schools/schools.js
@@ -1,4 +1,31 @@
-Template.registerHelper("schools", function (param2) {
-  console.log(Schools.find({}))
-  return Schools.find({});
+
+Template.manageSchools.helpers({
+    settings: function ()
+    {
+      return {
+          collection: Schools,
+          rowsPerPage: 10,
+          showFilter: true,
+          fields: [
+            { key: 'schoolDetails.INSTITUTION_NAME', label: 'Name' },
+            { key: 'schoolDetails.DISTRICT_NAME', label: 'District' },
+            // { key: 'edit', label: '', fn: function () { return new Spacebars.SafeString('<button type="button" class="editbtn">Edit</button>') } },
+            { key: 'delete', label: '', fn: function () { return new Spacebars.SafeString('<button type="button" class="deletebtn">Delete</button>') } }
+
+          ]
+
+      };
+    }
+});
+
+Template.schools.events({
+  'click .reactive-table tbody tr': function (event) {
+    event.preventDefault();
+
+    // checks if the actual clicked element has the class `delete`
+    if (event.target.className == "deletebtn") {
+      Schools.remove(this._id)
+    }
+  }
+
 });
