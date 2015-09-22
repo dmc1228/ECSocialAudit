33a68e2744b842a364ed4257cb5657828518f5a2
Carla Goldstein <carla.goldstein@gmail.com>
2015-09-22 19:04:21 +0200

    profile date working

--- client/views/audits/audit/formA/general.js
+++ client/views/audits/audit/formA/general.js
@@ -1,6 +1,7 @@
 
 Template.general.onRendered(function() {
   $('.datepicker').pickadate({
+   autoclose: true,
    selectMonths: true, // Creates a dropdown to control month
    selectYears: 15 // Creates a dropdown of 15 years to control year
  });
--- client/views/formgenerator/form.js
+++ client/views/formgenerator/form.js
@@ -14,3 +14,12 @@ Template.registerHelper('isActive', function(val) {
 
   return classes;
 })
+
+// Template.registerHelper('setDate', function(date) {
+//   // Using a string along with the parsing format (defaults to `format` option).
+//   // $('.datepicker').pickadate().set('select', '2016-04-20', { format: 'yyyy-mm-dd' })
+//   console.log(new Date(2008,9,03))
+//   $('.datepicker').datepicker("setDate", new Date(2008,9,03) );
+//   console.log($('.datepicker').datepicker())
+//
+// })
