fcfec2a4e87e12efb34f631e10261551e988ef8f
Carla Goldstein <carla.goldstein@gmail.com>
2015-09-25 09:13:53 +0200

    added forms and restructured files to represent forms

--- client/views/audits/audit/formA/formA.js
+++ client/views/audits/audit/formA/formA.js
@@ -8,9 +8,9 @@ formA = function (){
       security: {
         infrastructure: infrastructure,
         planning: planning,
-        // violence: violence,
-        // staff: security_staff
-        // sense: sense
+		    violence: violence,
+        staff: security_staff,
+        sense: sense
       }
 
     }
@@ -99,7 +99,7 @@ var grades = [
   {
       id: 'grade12',
       name: 'Grade 12'
-  }]
+  }];
 
 var staff = [
   {
@@ -121,7 +121,7 @@ var staff = [
   {
     id: 'number_of_maintenance_staff',
     name: 'Number of maintenance staff'
-  }]
+  }];
 
 var infrastructure = [
   {
@@ -135,8 +135,7 @@ var infrastructure = [
     type: 'dropdown',
     label: 'Does the school have an alarm?',
     options: ['Yes, and it works', 'Yes, but it doesn\'t work', 'No', 'Don\'t know']
-  }
-]
+  }];
 
 var planning = [
   {
@@ -146,9 +145,120 @@ var planning = [
     options: ['Yes', 'No', 'Don\'t know']
   },
   {
-    id: 'active_safety_committee',
+    id: 'safety_committee_activities',
     type: 'dropdown',
-    label: 'Is the Safety Committee active at the school?',
-    options: ['Yes (please describe in comments)', 'No, no committee exists', 'I don\'t know what they do']
-  }
-]
+    label: 'Are you aware of the activities of the safety committee?',
+    options: ['Yes (please describe in comments)', 'No, no committee exists', 'No, a committee exists, but I don\'t know what they do']
+  },
+  {
+    id: 'safety_plan',
+    type: 'dropdown',
+    label: 'Does the school have a safety plan?',
+    options: ['Yes', 'No', 'Don\'t know']
+  },
+  {
+    id: 'safety_plan_policy',
+    type: 'dropdown',
+    label: 'Are you aware of the policies of the safety plan?',
+    options: ['Yes (please describe in comments)', 'No, no plan exists', 'No, a plan exists, but I don\'t know what they do']
+  },
+  {
+    id: 'safety_officer',
+    type: 'dropdown',
+    label: 'Does the school have a school safety officer?',
+    options: ['Yes', 'No', 'Don\'t know']
+  },
+  {
+    id: 'safety_officer_activities',
+    type: 'dropdown',
+    label: 'Are you aware of the activities of the school safety officer?',
+    options: ['Yes (please describe in comments)', 'No, there is no school safety officer', 'No, there is an officer, but I don\'t know what they do']
+  },
+  {
+    id: 'conduct_code',
+    type: 'dropdown',
+    label: 'Does the school have a code of conduct"?',
+    options: ['Yes', 'No', 'Don\'t know']
+  },
+  {
+    id: 'code_policies',
+    type: 'dropdown',
+    label: 'Are you aware of the policies of the code of conduct?',
+    options: ['Yes (please describe in comments)', 'No, no code exists', 'No, a code exists, but I don\'t know what they do']
+  }];
+
+var violence = [
+  {
+    id: 'violence_3months',
+    type: 'checkbox',
+    label: 'In the last 3 months, which of the following events have taken place at the school and been reported to the administration?',
+    options: ['Theft of personal property', 'Gang violence', 'Sexual harassment', 'Alcohol use', 'Drug use', 'Physical violence against an educator', 'Physical violence against a learner', 'Not answered']
+  },
+  {
+    id: 'violence_1year',
+    type: 'checkbox',
+    label: 'In the last year, which of the following events have taken place at the school and been reported to the administration?',
+    options: ['Physical violence resulting in significant injury of an educator', 'Physical violence resulting in signficant injury of a learner', 'A rape', 'A shooting', 'A stabbing', 'Significant vandalism', 'Burglary', 'Not answered']
+  },
+  {
+    id: 'reported',
+    type: 'dropdown',
+    label: 'How regularly are there reported cases of burglaries and significant vandalism?',
+    options: ['Never Almost Never', 'Once a year', 'Every 6 months', 'Once a month', 'Weekly', 'Daily', 'Don\’t know']
+  }];
+
+var security_staff = [
+  {
+    id: 'teacher_training',
+    type: 'dropdown',
+    label: 'When did teachers last receive safety training?',
+    options: ['In the last 6 months', 'In the last year', 'In the last 2 years', 'More than 2 years ago', 'Never', 'Don\'t know']
+  },
+  {
+    id: 'hired_guards',
+    type: 'dropbox',
+    label: 'Who hires the security guard(s)?',
+    options: ['There is no security guard', 'WCED', 'SGB', 'Principal', 'Don\'t know', 'Other']
+  },
+  {
+    id: 'guard_trained',
+    type: 'dropdown',
+    label: 'Are the security guards trained?',
+    options: ['There is no security guard', 'Yes', 'No', 'Don\’t know']
+  },
+  {
+    id: 'defensive_instrument',
+    type: 'checkbox',
+    label: 'What type of defensive instrument does the security guard have?',
+    options: ['There is no security guard', 'The security guard does not have a defensive instrument', 'Gun', 'Baton', 'Knobkerrie', 'Sjambok', 'Don\t know', 'Other']
+  },
+  {
+    id: 'body_search',
+    type: 'dropdown',
+    label: 'Does the school conduct body searches?',
+    options: ['Yes, they are conducted by police with police dogs', 'Yes, they are conducted by police without dogs', 'Yes, they are conducted by staff', 'No', 'Don\’t know']
+  },
+  {
+    id: 'body_search_gender',
+    type: 'dropdown',
+    label: 'If body searches are conducted, is it required that they are they done by an official of the same gender as the learner being searched (e.g. men search boys and women search girls)?',
+    options: ['Yes, it is required and searches are always done by the same gender', 'Yes, it is required but searches are not always done by the same gender', 'No, it is not required', 'No, we do not conduct body searches', 'Don\’t know']
+  },
+  {
+    id: 'bag_search',
+    type: 'dropdown',
+    label: 'Does the school conduct bag searches?',
+    options: ['Yes, they are conducted by police with police dogs', 'Yes, they are conducted by police without dogs', 'Yes, they are conducted by staff', 'No', 'Don\’t know']
+  }];
+
+var sense = [
+  {
+    id: 'safe_feel',
+    type: 'dropdown',
+    label: 'How safe do you feel learners and educators are at the school',
+    options: ['Very Safe','Safe', 'Neither Safe Nor Unsafe', 'Unsafe', 'Very Unsafe']
+  },
+  {
+    id: 'safety_challenges',
+    name: 'Please discuss what you [the adminstrator] think the challenges to safety are at the school. Record their answer below'
+  }]
---
+++ client/views/audits/audit/formC/formC.js
@@ -0,0 +1,105 @@
+formC = function (){
+    return {
+      school_demographics : {
+        general: generalQuestions,
+      },
+
+	  general_infrastructure : {
+        fencing: fencing,
+		building_condition : building_condition,
+		sanitation1: sanitation1,
+		// sanitation2: sanitation2,
+      },
+
+	   general_atmosphere : {
+        atmosphere: atmosphere
+      },
+
+    }
+}
+
+var generalQuestions = [
+  {
+    id: 'name_of_auditor',
+    type: 'text',
+    label: 'Name of Auditor'
+  },
+  {
+    id: 'survey_date',
+    type: 'date',
+	label: 'Date of Survey'
+  }];
+
+var fencing = [
+  {
+    id: 'have_fence',
+    type: 'dropdown',
+    label: 'Does the school have a fence?',
+    options: ['Yes', 'No', 'Don\'t know']
+  },
+  {
+    id: 'fence_holes',
+    type: 'dropdown',
+    label: 'Does the fence have holes in it?',
+    options: ['Yes', 'No', 'Don\'t know']
+  },
+  {
+    id: 'tall_fence',
+    type: 'dropdown',
+    label: 'Is the fence at least 1.8 meters tall?',
+    options: ['Yes', 'No', 'Don\'t know']
+  },
+  {
+    id: 'secure_fence',
+    type: 'dropdown',
+    label: 'Do you think that someone, including learners, would be able to get through the fence?',
+    options: ['Yes', 'No', 'Maybe', 'Don\'t know']
+  },
+  {
+    id: 'secure_fence',
+    type: 'checkbox',
+    label: 'What is the fence made out of?',
+    options: ['Concrete', 'Brick', 'Wood', 'Wire', 'Metal, with a flat top', 'Metal, with spike', 'Other']
+  }];
+
+var building_condition = [
+  {
+    id: 'total_number_of_structures',
+    name: 'Total number of structures'
+  },
+  {
+    id: 'number_of_inappropriate_structures',
+    name: 'Number of structures made out of inappropriate material'
+  }]
+
+  var sanitation1 = [
+  {
+    id: 'total_toilet_blocks',
+    name: 'Total number of toilet blocks'
+  },
+  {
+    id: 'blocks_locked',
+    name: 'Total number of toilet blocks that are locked'
+  },
+  {
+    id: 'total_taps_outside',
+    name: 'Total number of taps outside of toilet blocks'
+  },
+  {
+    id: 'total_taps_outside',
+    name: 'Total number of taps outside of toilet blocks'
+  }];
+
+  var atmosphere = [
+  {
+    id: 'feel_safe',
+    type: 'dropdown',
+    label: 'How safe did you feel at the school?',
+    options: ['Very Safe', 'Safe', 'Neither Safe Nor Unsafe', 'Unsafe', 'Very Unsafe']
+  },
+  {
+    id: 'cooperative',
+    type: 'dropdown',
+    label: 'How cooperative was the school administration?',
+    options: ['Very cooperative', 'Cooperative', 'Somewhat cooperative', 'Not cooperative', 'Very uncooperative']
+  }]
