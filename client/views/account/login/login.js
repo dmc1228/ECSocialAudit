79f6270c84b90ed1dc8b8e225d9d680289388b09
Carla Goldstein <carla.goldstein@gmail.com>
2015-10-02 00:44:29 +0200

    using native login added user roles

--- client/views/account/login/login.js
+++ client/views/account/login/login.js
@@ -18,7 +18,7 @@ var isValidPassword = function(val, field) {
         e.preventDefault();
         // retrieve the input field values
         var email = t.find('#login-email').value
-          , password = t.find('#login-password').value;
+        var password = t.find('#login-password').value;
 
           // Trim
           email = trimInput(email);
@@ -28,18 +28,15 @@ var isValidPassword = function(val, field) {
 
           // If validation passes, supply the appropriate fields to the
           Meteor.loginWithPassword(email, password, function(err){
-          if (err){
-            console.log(err)
-            FlashMessages.sendError(err.reason);
-            return;
-
-          }
-          console.log(Router.current().route.name)
-          if (Router.current().route.name === 'accounts') {
+            if (err){
+              console.log(err)
+              FlashMessages.sendError(err.reason);
+              return;
+            }
             console.log("User logged in");
             FlashMessages.sendSuccess("Successfully logged in");
             return Router.go("/audits");
-          }
+
         });
            return false;
       }
---
+++ client/views/users/users.js
@@ -0,0 +1,43 @@
+var updatedUsers = [];
+
+Template.registerHelper('users',function(){
+  var users = Meteor.users.find();
+  return users;
+});
+
+Template.registerHelper('compareUserIds',function(checkboxId){
+  var currentUserId = Meteor.userId();
+  var admin = Meteor.users.findOne({'emails.address' : {$in: ['carla@equaleducation.org.za']}});
+  console.log(checkboxId)
+  if (currentUserId == checkboxId || admin._id == checkboxId) {
+    return true;
+  }
+  return false;
+});
+
+Template.registerHelper('checkRole', function(userId, roleToCheck) {
+  var userWithRoles = Meteor.users.findOne({"_id" : userId}, {"roles" : 1});
+  var hasRole = false;
+  if (userWithRoles.roles != undefined) {
+    var index = userWithRoles.roles.indexOf(roleToCheck);
+    console.log(roleToCheck);
+    console.log(index)
+    if (index > -1) {
+      hasRole = true;
+    }
+  }
+  return hasRole;
+});
+
+Template.users.events({
+  'click #checkbox': function(event, template) {
+    Meteor.call('toggleUserRole', this._id, event.target.value, function(error, result) {
+      if (error) {
+        FlashMessages.sendError("Failed to update users");
+        console.log(error);
+      } else {
+        FlashMessages.sendSuccess("Updated user");
+      }
+    })
+  }
+})
