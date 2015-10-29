46738fa4679aaea8c95bba7f3ba8f0e3dbf37f1f
David Carel <dmc1228@gmail.com>
2015-10-29 12:05:09 +0200

    added frontend components

--- client/views/ECPLan/ecplan.js
+++ client/views/ECPLan/ecplan.js
@@ -1,8 +1,46 @@
-
 Template.ecplan.helpers({
+  
+  schoolsInPlan: [
+    
+    { 
+      schoolName:"School 1",
+      projectID: "ID123" ,
+      agent: "coega" ,
+      source: "Infrastructure Grant" ,
+      description: "toilet",
+      stage:"year",
+      start:"July",
+      end:"August",
+      totalcost:"R2000",
+      budgetYear:2015,
+      comments:"test comment"
+    }
+    
+      ]
     
 });
 
+
+
 Template.ecplan.events({
+  
+
+  "submit .schoolSearch": function (event) {
+    // Prevent default browser form submit
+    event.preventDefault();
+
+    // Get value from form element
+    var text = event.target.text.value;
+
+    // Insert a task into the collection
+    Tasks.insert({
+      text: text,
+      createdAt: new Date() // current time
+    });
+
+    // Clear form
+    event.target.text.value = "";
+  }
 
 });
+
