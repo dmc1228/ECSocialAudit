f8a9ce532c341481d2e91eab7e179a33364a32d5
David Carel <dmc1228@gmail.com>
2015-11-06 15:09:42 +0200

    adding live search

--- client/views/ECPLan/ecplan.js
+++ client/views/ECPLan/ecplan.js
@@ -11,16 +11,46 @@ if (Meteor.isClient) {
   });
 
 
-
   Template.ecplan.events({
-     
-  
- // Meteor.methods({    
-//    checkSchool: function (school) {            
-    
-  //});
+    'keyup input.schoolSearch': function (evt) {
+      Session.set("search-query", evt.currentTarget.value);
+    }
+  })
 
-});
+
+  Template.schoolInPlan.helpers({
+    
+    searchResults: function () { 
+        var keyword  = Session.get("search-query");
+        var query = new RegExp( keyword, 'i' );
+        console.log(query);
+        var results = ECPlans.find( { 'schoolName':query } );
+        console.log(results);
+        return {results: results};
+      }
+
+    
+  });
+
+  
+}
+
+
+if (Meteor.isServer) {
+
+  Meteor.startup(function() {
+
+    return Meteor.methods({
+
+      removeAll: function() {
+
+        return ECPlans.remove({});
+
+      }
+
+    });
+
+  });
 
 }
 
